3b7f4f868be2cbf87e6a743aa25f9a2f9fbd711c
truvishsolutions-hub <truvishsolutions@gmail.com>
2026-04-26 09:54:43 +0530

    Fix API URL and add logo asset

--- src/components/AdminClientUI.jsx
+++ src/components/AdminClientUI.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./AdminClientUI.css";
 import truvishLogo from "../assets/LOGO/TV-BG.png";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://truvish-backend-production.up.railway.app";
 const MAX_IMAGE_SIZE_BYTES = 1024 * 1024;
 
 const CATEGORY_OPTIONS = [
